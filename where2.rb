f2c354f0cbd0ef953796fb300582cbcb3b0b8b05
Brian Eng <brian@brianeng.com>
2020-02-14 09:16:13 -0600

    initial commit

--- where2.rb
+++ where2.rb
@@ -5,11 +5,5 @@ before { puts "Parameters: #{params}" }
 
 get "/" do
   # lat: ± 90
-  lat = rand(-90.0..90)
   # long: ± 180
-  long = rand(-180.0..180)
-  
-  @ll = "#{lat},#{long}"
-
-  view "where2"
-end 
+end
