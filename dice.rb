56db2fe6d3c7da7159dcded24dcd5c5d805a75b5
wsiroky <wsiroky@gmail.com>
2020-02-18 02:23:24 +0000

    more from class

--- dice.rb
+++ dice.rb
@@ -4,5 +4,7 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
-  "Hello"
+    @die1 = rand(1...6)
+    @die2 = rand(1...6)
+    view "dice"
 end
