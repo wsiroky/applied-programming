a00eeae74c477cc5d56439f11c056f186675e588
wsiroky <wsiroky@gmail.com>
2020-02-18 02:36:33 +0000

    more from class

--- where2.rb
+++ where2.rb
@@ -4,6 +4,7 @@ def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }  
 
 get "/" do
-  # lat: ± 90
-  # long: ± 180
+  @lat = rand(-90.000...90.000)
+  @long = rand(-180.000...180.000)
+  view "where2"
 end
